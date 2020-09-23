686be6f9c7f8dc4b2acce34eedef05030c1a012e
Adrian1426 <adrianhh1426@gmail.com>
2020-09-22 23:00:40 -0500

    React reveal dependencia

---
+++ animaciones-transiciones/animaciones-transiciones/src/ReactReveal/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Zoom from 'react-reveal/Zoom';
+
+const App = () => {
+  return (
+    <div>
+      <section>
+        <h3>Adrian Hernández Hernández</h3>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas est cum esse obcaecati laudantium perspiciatis veritatis omnis dolorum ab dolores nemo sunt quas, soluta sapiente nisi unde nihil quos eius?</p>
+      </section>
+
+      <section>
+        <h3>Adrian Hernández Hernández</h3>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas est cum esse obcaecati laudantium perspiciatis veritatis omnis dolorum ab dolores nemo sunt quas, soluta sapiente nisi unde nihil quos eius?</p>
+      </section>
+
+      <section>
+        <h3>Adrian Hernández Hernández</h3>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas est cum esse obcaecati laudantium perspiciatis veritatis omnis dolorum ab dolores nemo sunt quas, soluta sapiente nisi unde nihil quos eius?</p>
+      </section>
+
+      <section>
+        <h3>Adrian Hernández Hernández</h3>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas est cum esse obcaecati laudantium perspiciatis veritatis omnis dolorum ab dolores nemo sunt quas, soluta sapiente nisi unde nihil quos eius?</p>
+      </section>
+
+      <section>
+        <h3>Adrian Hernández Hernández</h3>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas est cum esse obcaecati laudantium perspiciatis veritatis omnis dolorum ab dolores nemo sunt quas, soluta sapiente nisi unde nihil quos eius?</p>
+      </section>
+
+      <section>
+        <h3>Adrian Hernández Hernández</h3>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas est cum esse obcaecati laudantium perspiciatis veritatis omnis dolorum ab dolores nemo sunt quas, soluta sapiente nisi unde nihil quos eius?</p>
+      </section>
+
+      <Zoom>
+        <section>
+          <h3>Adrian Hernández Hernández</h3>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas est cum esse obcaecati laudantium perspiciatis veritatis omnis dolorum ab dolores nemo sunt quas, soluta sapiente nisi unde nihil quos eius?</p>
+        </section>
+      </Zoom>
+    </div>
+  );
+};
+
+export default App;
--- animaciones-transiciones/animaciones-transiciones/src/index.js
+++ animaciones-transiciones/animaciones-transiciones/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Acordeon/App';
+import App from './ReactReveal/App';
 
 ReactDOM.render(
   <React.StrictMode>
