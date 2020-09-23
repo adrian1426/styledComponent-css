000124b4750d34bf6288b39d31b81254f1fd7505
Adrian1426 <adrianhh1426@gmail.com>
2020-09-22 21:29:14 -0500

    animaciones y transiciones en clases css

--- animaciones-transiciones/animaciones-transiciones/src/components/Header.js
+++ animaciones-transiciones/animaciones-transiciones/src/components/Header.js
@@ -2,22 +2,10 @@ import React from 'react';
 
 const Header = (props) => {
   const { show } = props;
-
-  const headerStyle = {
-    background: show ? '#3d1f9a' : 'black',
-    position: 'absolute',
-    textAlign: 'center',
-    borderRadius: '.4em',
-    color: '#FFF',
-    padding: '0.5em',
-    margin: '0.5em',
-    fontSize: '14px',
-    transition: 'all 800ms ease',
-    transform: show ? 'scale(1)' : 'scale(0)'
-  };
+  const estilo = `header ${show && 'header-active'} `;
 
   return (
-    <header style={headerStyle}>
+    <header className={estilo}>
       <h1>
         Transiciones css  en linea
         <span role="img" aria-label="fire">
